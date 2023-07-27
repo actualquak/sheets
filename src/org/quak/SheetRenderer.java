53e97fdab4be2e495128a27997a22cef4edcc9a2
actualquak <stanley.g.foster@gmail.com>
2023-07-27 11:22:43 +1000

    First bit here

--- src/org/quak/SheetRenderer.java
+++ src/org/quak/SheetRenderer.java
@@ -4,8 +4,10 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 public class SheetRenderer extends JComponent implements KeyListener {
-    public SheetRenderer(SheetFrame frame) { throw new NotYetImplemented(); }
-    public SheetRenderer(SheetFrame frame, SheetRegistry registry) { throw new NotYetImplemented(); }
+    SheetFrame frame;
+    SheetRegistry registry;
+    public SheetRenderer(SheetFrame frame) { this(frame, new SheetRegistry()); }
+    public SheetRenderer(SheetFrame frame, SheetRegistry registry) { this.frame = frame; this.registry = registry; }
     @Override public void keyTyped(KeyEvent keyEvent) { throw new NotYetImplemented(); }
     @Override public void keyPressed(KeyEvent keyEvent) { throw new NotYetImplemented(); }
     @Override public void keyReleased(KeyEvent keyEvent) { throw new NotYetImplemented(); }
